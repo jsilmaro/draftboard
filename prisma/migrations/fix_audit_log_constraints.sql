f65a3ce2f560c6e116b09ab0c512de28bbbf280f
Janelle <silmaro.janelle84686@gmail.com>
2025-09-08 21:18:36 +0800

    Cleaning the files unneeded

--- prisma/migrations/fix_audit_log_constraints.sql
+++
@@ -1,8 +0,0 @@
--- Fix AuditLog table constraints
-ALTER TABLE "AuditLog" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
-
--- Update existing records to have updatedAt value
-UPDATE "AuditLog" SET "updatedAt" = "createdAt" WHERE "updatedAt" IS NULL;
-
--- Make sure the constraint is properly set
-ALTER TABLE "AuditLog" ALTER COLUMN "updatedAt" SET NOT NULL;
